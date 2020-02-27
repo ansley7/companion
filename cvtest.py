3f400f8446e63ada22b7bc1c4c9ab02436276acd
yonip <yoni.pedersen@gmail.com>
2020-02-27 18:56:03 -0500

    Added a system that would check whether the COM of the target is close enough to cetner to not turn

--- cvtest.py
+++ cvtest.py
@@ -1,6 +1,7 @@
 import cv2
 import imutils
 from imutils.video import WebcamVideoStream as wvs
+import numpy as np
 
 hmin = 0
 hmax = 360
@@ -65,6 +66,7 @@ def largest_single_color(frame, hues):
     for hmin, hmax in hues:
         mask = cv2.inRange(frame, (hmin, smin, vmin), (hmax, vmax, smax))
         _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = list(filter(lambda cont: len([pt for pt in cont if pt[0][1] < 150]) == 0, contours))
         if len(contours) > 0:
             poss = max(contours, key=cv2.contourArea)
             
@@ -81,6 +83,7 @@ def largest_merge_colors(frame, hues):
         else:
             mask = cv2.bitwise_or(mask, cv2.inRange(frame, (hmin, smin, vmin), (hmax, vmax, smax)))
     _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = list(filter(lambda cont: len([pt for pt in cont if pt[0][1] < 150]) == 0, contours))
     if len(contours) > 0:
         cont = max(contours, key=cv2.contourArea)
         lineColor = (5, 240, 240)
@@ -94,6 +97,38 @@ def get_black_image(frame):
     return cv2.bitwise_or(frame, frame, mask=out)
     
 
+def draw_cont(img, cont, color):
+    m = cv2.moments(cont)
+    if m["m00"] != 0:
+        cv2.circle(img, (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"])), 5, color, -1)
+    cv2.drawContours(img, [cont], 0, color, 3)
+
+def check_cont(cont):
+    return cv2.approxPolyDP(cont, 0.005*cv2.arcLength(cont, True), True)
+
+def get_side(cont):
+    m = cv2.moments(cont)
+    if m["m00"] == 0:
+        return None
+    y = int(m["m01"]/m["m00"])
+    x = int(m["m10"]/m["m00"])
+    if -(x*91/95) + 127+(480*91/95) > y:
+        return -1 # left
+    if x*91/95 + 127-(480*91/95) > y:
+        return 1 # right
+    return 0 # forward
+    
+side_color = {
+    -1: (300, 240, 240),
+    0: (0, 0, 255),
+    1: (120, 240, 240)
+    }
+
+# y - y1 = m(x-x1)
+# y = mx - mx1 + y1
+# y = mx + (y1 - mx1)
+# (0, y1 - mx1), (w, mw + y1 - mx1
+
 def main():
     #global hmin
     #global hmax
@@ -103,16 +138,24 @@ def main():
     
     #cv2.createTrackbar("Hue: red, orange, lime, green, sky", "res", 0, 4, set_hue)
     #cv2.createTrackbar("H max", "res", hmax, 360, set_hmax)
-    cv2.createTrackbar("S min", "res", smin, 255, set_smin)
-    cv2.createTrackbar("S max", "res", smax, 255, set_smax)
-    cv2.createTrackbar("V min", "res", vmin, 255, set_vmin)
-    cv2.createTrackbar("V max", "res", vmax, 255, set_vmax)
-    v = wvs(src=1).start()
+    #cv2.createTrackbar("S min", "res", smin, 255, set_smin)
+    #cv2.createTrackbar("S max", "res", smax, 255, set_smax)
+    #cv2.createTrackbar("V min", "res", vmin, 255, set_vmin)
+    #cv2.createTrackbar("V max", "res", vmax, 255, set_vmax)
+    v = cv2.VideoCapture(1)
+    cap_width = 1920
+    cap_height = 1080
+    w = int(cap_width/2)
+    h = int(cap_height/2)
+    v.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_width)
+    v.set(cv2.CAP_PROP_FRAME_WIDTH, cap_height)
+    v.set(cv2.CAP_PROP_FPS, 24)
     while True:
-        frame = v.read()
+        _, frame = v.read()
         if frame is None:
             break
 
+        frame = cv2.resize(frame, (w,h), interpolation=cv2.INTER_AREA)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         #mask = cv2.inRange(frame, (hmin, smin, vmin), (hmax, vmax, smax))
         #frame = cv2.bitwise_or(frame, frame, mask=mask)
@@ -121,9 +164,22 @@ def main():
     
         cont, color = largest_single_color(frame, hues)
         if cont is not None:
+            draw_cont(out, cont, color)
+            cont = check_cont(cont)
+            draw_cont(out, cont, color)
+        cont, color = largest_merge_colors(frame, hues)
+        if cont is not None:
+            draw_cont(out, cont, color)
             m = cv2.moments(cont)
-            cv2.circle(out, (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"])), 5, color, -1)
-            cv2.drawContours(out, [cont], 0, color, 3)
+            side = get_side(cont)
+            if side is not None:
+                cv2.line(out, (int(m["m10"] / m["m00"]), 0), (int(m["m10"] / m["m00"]), 480), side_color[side], 2)
+            cont = check_cont(cont)
+            draw_cont(out, cont, color)
+        cv2.line(out, (0, int(0.3125*h)), (w, int(0.3125*h)), (0, 0, 0), 1)
+        cv2.line(out, (int(w/2), 0), (int(w/2), h), (0, 0, 0), 1)
+        cv2.line(out, (0, int(127+(w/2)*91/95)), (w, int(-(w*91/95) + 127+(w/2)*91/95)), (0, 240, 240), 1)
+        cv2.line(out, (0, int(127-(w/2)*91/95)), (w, int((w*91/95)+127-(w/2)*91/95)), (0, 240, 240), 1)
         out = cv2.cvtColor(out, cv2.COLOR_HSV2BGR)
         cv2.imshow("res", out)
 
@@ -134,3 +190,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# at 220, (420-230) = 190 -> 1ft
+# at 260, (460-190) = 270 -> 1ft
+# at 350, (555-90) = 465 -> 1ft
+# at 400, (605-40) = 565 -> 1ft
