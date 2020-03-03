3441d0dbfc8dd08976e7481e388586d9776b8e3b
yonip <yoni.pedersen@gmail.com>
2020-03-03 13:53:42 -0500

    Move lego detection code into an object, add debug text

--- cvtest.py
+++ cvtest.py
@@ -2,138 +2,149 @@ import cv2
 import imutils
 from imutils.video import WebcamVideoStream as wvs
 import numpy as np
-
-hmin = 0
-hmax = 360
-smin = 50
-smax = 255
-vmin = 110
-vmax = 255
-
-red_hue = 0, 7
-orange_hue = 10, 20
-lime_hue = 33, 48
-sky_hue = 92, 105
-green_hue = 65, 79
-
-hues = (red_hue, orange_hue, lime_hue, green_hue, sky_hue)
-
-def set_hue(x):
-    global hmin
-    global hmax
-    hmin, hmax = hues[x]
-
-def set_hmin(x):
-    global hmin
-    hmin = x
-
-def set_hmax(x):
-    global hmax
-    hmax = x
-
-def set_smin(x):
-    global smin
-    smin = x
-
-def set_smax(x):
-    global smax
-    smax = x
-
-def set_vmin(x):
-    global vmin
-    vmin = x
-
-def set_vmax(x):
-    global vmax
-    vmax = x
-
-def get_color(h):
-    if h < 8:
-        # red
-        return (0, 255, 0)
-    if h < 25:
-        # orange
-        return (230, 102, 23)
-    if h < 50:
-        # lime
-        return
-
-def largest_single_color(frame, hues):
-    largest = None
-    poss = None
-    lineColor = None
-    
-    for hmin, hmax in hues:
-        mask = cv2.inRange(frame, (hmin, smin, vmin), (hmax, vmax, smax))
-        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = list(filter(lambda cont: len([pt for pt in cont if pt[0][1] < 150]) == 0, contours))
-        if len(contours) > 0:
-            poss = max(contours, key=cv2.contourArea)
-            
-            if largest is None or cv2.contourArea(poss) > cv2.contourArea(largest):
-                largest = poss
-                lineColor = (int(hmin + (hmax-hmin)/2), 240, 240)
-    return largest, lineColor
-
-def largest_merge_colors(frame, hues):
-    mask = None
-    for hmin, hmax in hues:
-        if mask is None:
-            mask = cv2.inRange(frame, (hmin, smin, vmin), (hmax, vmax, smax))
-        else:
-            mask = cv2.bitwise_or(mask, cv2.inRange(frame, (hmin, smin, vmin), (hmax, vmax, smax)))
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = list(filter(lambda cont: len([pt for pt in cont if pt[0][1] < 150]) == 0, contours))
-    if len(contours) > 0:
-        cont = max(contours, key=cv2.contourArea)
-        lineColor = (5, 240, 240)
-    else:
-        cont = None
-        lineColor = None
-    return (cont, lineColor)
+from typing import Iterable, Tuple
 
 def get_black_image(frame):
     out = cv2.inRange(frame, (0, 0, 0), (0, 0, 0))
     return cv2.bitwise_or(frame, frame, mask=out)
-    
-
-def draw_cont(img, cont, color):
-    m = cv2.moments(cont)
-    if m["m00"] != 0:
-        cv2.circle(img, (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"])), 5, color, -1)
-    cv2.drawContours(img, [cont], 0, color, 3)
 
 def check_cont(cont):
     return cv2.approxPolyDP(cont, 0.005*cv2.arcLength(cont, True), True)
-
-def get_side(cont):
-    m = cv2.moments(cont)
-    if m["m00"] == 0:
-        return None
-    y = int(m["m01"]/m["m00"])
-    x = int(m["m10"]/m["m00"])
-    if -(x*91/95) + 127+(480*91/95) > y:
-        return -1 # left
-    if x*91/95 + 127-(480*91/95) > y:
-        return 1 # right
-    return 0 # forward
-    
-side_color = {
-    -1: (300, 240, 240),
-    0: (0, 0, 255),
-    1: (120, 240, 240)
-    }
 
 # y - y1 = m(x-x1)
 # y = mx - mx1 + y1
 # y = mx + (y1 - mx1)
 # (0, y1 - mx1), (w, mw + y1 - mx1
 
+class LegoTracker:
+    def __init__(self,
+                 hues: Iterable[Tuple[int,int]],
+                 saturation_min: int,#Tuple[int,int],
+                 value_min: int,#Tuple[int,int],
+                 boundary_slope: float,
+                 boundary_yint: int,
+                 horizon: int,
+                 width: int,
+                 height: int,
+                 find_single: bool,
+    ):
+        self.hues = hues
+        self.saturation_range = (saturation_min, 255)
+        self.value_range = (value_min, 255)
+        self.boundary_slope = boundary_slope
+        self.boundary_yint = boundary_yint
+        self.horizon = horizon
+        self.width = width
+        self.height = height
+        self.find_single = find_single
+        self.current_direction = None
+        self.side_color = {
+            -1: (300, 240, 240),
+            0: (0, 0, 255),
+            1: (120, 240, 240)
+            }
+
+    def largest_merge(self, frame):
+        mask = None
+        smin, smax = self.saturation_range
+        vmin, vmax = self.value_range
+        for hmin, hmax in self.hues:
+            if mask is None:
+                mask = cv2.inRange(frame, (hmin, smin, vmin), (hmax, smax, vmax))
+            else:
+                mask = cv2.bitwise_or(mask, cv2.inRange(frame, (hmin, smin, vmin), (hmax, smax, vmax)))
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = filter(lambda cont: len([pt for pt in cont if pt[0][1] < self.horizon]) == 0, contours)
+        cont = max(contours, key=cv2.contourArea, default=None)
+        return cont, (5, 240, 240)
+
+    def largest_single(self, frame):
+        largest = None
+        lineColor = None
+        smin, smax = self.saturation_range
+        vmin, vmax = self.value_range
+        for hmin, hmax in self.hues:
+            mask = cv2.inRange(frame, (hmin, smin, vmin), (hmax, smax, vmax))
+            _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours = filter(lambda cont: len([pt for pt in cont if pt[0][1] < self.horizon]) == 0, contours)
+            poss = max(contours, key=cv2.contourArea, default=None)
+            if poss is not None and (largest is None or cv2.contourArea(poss) > cv2.contourArea(largest)):
+                largest = poss
+                lineColor = (int(hmin + (hmax-hmin)/2), 240, 240)
+        return largest, lineColor
+
+    def get_side(self, moment):
+        if moment["m00"] == 0:
+            return None
+        y = int(moment["m01"]/moment["m00"])
+        x = int(moment["m10"]/moment["m00"])
+        if -x*self.boundary_slope + self.boundary_yint + ((self.width/2)*self.boundary_slope) > y:
+            return -1 # left
+        if x*self.boundary_slope + self.boundary_yint - ((self.width/2)*self.boundary_slope) > y:
+            return 1 # right
+        return 0
+
+    def get_contour(self, frame):
+        if self.find_single:
+            return self.largest_single(frame)
+        else:
+            return self.largest_merge(frame)
+    
+    def draw_contour(self, frame, cont, color):
+        if cont is not None:
+            m = cv2.moments(cont)
+
+            # draw COM and direction line
+            if m["m00"] != 0:
+                cx = int(m["m10"] / m["m00"])
+                cy = int(m["m01"] / m["m00"])
+                side = self.get_side(m)
+                if side is not None:
+                    cv2.line(frame, (cx, 0), (cx, self.height), self.side_color[side], 2)
+
+                cv2.circle(frame, (cx, cy), 5, color, -1)
+            cv2.drawContours(frame, [cont], 0, color, 3)
+            
+    def draw_guides(self, frame):
+        # horizon
+        cv2.line(frame, (0, self.horizon), (self.width, self.horizon), (0, 0, 0), 1)
+        # center line
+        cv2.line(frame, (int(self.width/2), 0), (int(self.width/2), self.height), (0, 0, 0), 1)
+
+        # boundary lines
+        # left
+        cv2.line(frame, (0, int(self.boundary_yint+((self.width/2)*self.boundary_slope))), (self.width, int(-self.width*self.boundary_slope + self.boundary_yint+((self.width/2)*self.boundary_slope))), (0, 240, 240), 1)
+        # right
+        cv2.line(frame, (0, int(self.boundary_yint-((self.width/2)*self.boundary_slope))), (self.width, int(self.width*self.boundary_slope + self.boundary_yint-((self.width/2)*self.boundary_slope))), (0, 240, 240), 1)
+
+    def write_text(self, frame):
+        
+        directions = {
+            -1: "Left",
+            0: "Forward",
+            1: "Right",
+            None: "Search",
+        }
+
+        direction = "Direction: " + directions[self.current_direction]
+        
+        color = self.side_color.get(self.current_direction, (0, 240, 240))
+
+        cv2.putText(frame, direction, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+        
+    def update(self, frame, draw=False):
+        cont, color = self.get_contour(frame)
+        self.current_direction = self.get_side(cv2.moments(cont))
+        if draw:
+            self.draw_contour(frame, cont, color)
+            self.draw_guides(frame)
+            self.write_text(frame)
+
 def main():
     #global hmin
     #global hmax
     cv2.namedWindow("res")
-
+    
     #hmin, hmax = red_hue
     
     #cv2.createTrackbar("Hue: red, orange, lime, green, sky", "res", 0, 4, set_hue)
@@ -147,9 +158,22 @@ def main():
     cap_height = 1080
     w = int(cap_width/2)
     h = int(cap_height/2)
+
+    red_hue = 0, 7
+    orange_hue = 10, 20
+    lime_hue = 33, 48
+    sky_hue = 92, 105
+    green_hue = 65, 79
+    
+    hues = (red_hue, orange_hue, lime_hue, green_hue, sky_hue)
+
+    tracker = LegoTracker(hues, 50, 110, (91/95), 127, int(0.3125 * h), w, h, True)
+
     v.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_width)
     v.set(cv2.CAP_PROP_FRAME_WIDTH, cap_height)
     v.set(cv2.CAP_PROP_FPS, 24)
+    
+    debug = True
     while True:
         _, frame = v.read()
         if frame is None:
@@ -161,30 +185,21 @@ def main():
         #frame = cv2.bitwise_or(frame, frame, mask=mask)
         #frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
         out = frame
-    
-        cont, color = largest_single_color(frame, hues)
-        if cont is not None:
-            draw_cont(out, cont, color)
-            cont = check_cont(cont)
-            draw_cont(out, cont, color)
-        cont, color = largest_merge_colors(frame, hues)
-        if cont is not None:
-            draw_cont(out, cont, color)
-            m = cv2.moments(cont)
-            side = get_side(cont)
-            if side is not None:
-                cv2.line(out, (int(m["m10"] / m["m00"]), 0), (int(m["m10"] / m["m00"]), 480), side_color[side], 2)
-            cont = check_cont(cont)
-            draw_cont(out, cont, color)
-        cv2.line(out, (0, int(0.3125*h)), (w, int(0.3125*h)), (0, 0, 0), 1)
-        cv2.line(out, (int(w/2), 0), (int(w/2), h), (0, 0, 0), 1)
-        cv2.line(out, (0, int(127+(w/2)*91/95)), (w, int(-(w*91/95) + 127+(w/2)*91/95)), (0, 240, 240), 1)
-        cv2.line(out, (0, int(127-(w/2)*91/95)), (w, int((w*91/95)+127-(w/2)*91/95)), (0, 240, 240), 1)
+
+        tracker.update(out, draw=debug)
+        
+        # draw the image - convert to BGR since cv2 uses bgr to draw
         out = cv2.cvtColor(out, cv2.COLOR_HSV2BGR)
         cv2.imshow("res", out)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        inp = cv2.waitKey(1) & 0xFF
+        if inp == ord("q"):
             break
+        if inp == ord("s"):
+            tracker.find_single = not tracker.find_single
+        if inp == ord("d"):
+            debug = not debug
+
     #v.release()
     cv2.destroyAllWindows()
 
